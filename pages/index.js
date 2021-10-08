30ea7ac452a74a9f79ea0dc435d701b238d0b5fe
Milan Steiner <steiner@travix-media.de>
2021-10-08 14:17:51 +0200

    Frontend Stuff, Voting Buttons

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Web3 from 'web3'
 import { useState, useEffect } from 'react'
 import { abi, contractAddress } from '../constants/abi';
+import { Button, Container, Row, Col, Table, InputGroup, FormControl } from 'react-bootstrap';
 
 export default function Home() {
 
@@ -14,7 +15,7 @@ export default function Home() {
   const [bestHello, setBestHello] = useState(null) // Highest voted Hello State
 
   useEffect(() => {
-    
+
     // Get eth accounts & contract
     window.ethereum ?
       ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
@@ -22,7 +23,7 @@ export default function Home() {
         let w3 = new Web3(ethereum)
         let c = new w3.eth.Contract(abi, contractAddress)
         setContract(c)
-
+        getHelloListing(c)
         // Else throw error
       }).catch((err) => console.log(err))
       : console.log("Please install MetaMask")
@@ -31,8 +32,8 @@ export default function Home() {
   /**
    * Create a new hello string
    */
-  function createHello() {
-    contract.methods.CreateHello('Hello World').send({from:address})
+  function createHello(hello) {
+    contract.methods.CreateHello(hello).send({ from: address })
   }
 
   /**
@@ -45,8 +46,17 @@ export default function Home() {
   /**
    * Get all submited hellos including their votings
    */
-  function GetHelloListing() {
-    contract.methods.GetHelloListing().call().then(result => setAllHellos(result))
+  function getHelloListing(c) {
+    c = c ?? contract
+    c.methods.GetHelloListing().call().then(result => setAllHellos(result))
+  }
+
+  /**
+   * Vote for a hello
+   */
+  function voteHello(hello) {
+    contract.methods.VoteHello(hello).send({ from: address })
+    getHelloListing();
   }
 
   // Markup
@@ -57,10 +67,41 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Hello</h1>
-        <button onClick={createHello}>Create Hello!</button>
-        <button onClick={getHello}>Get most voted Hello!</button>
-        <button onClick={GetHelloListing}>Get all Hellos!</button>
+        <Container>
+          <Row>
+            <h1>Hello World Voting!</h1>
+            <InputGroup className="mb-3">
+              <FormControl
+                defaultValue="New Hello World"
+                id="test"
+              />
+              <Button onClick={() => createHello(document.getElementById('test').value)} variant="outline-secondary">
+                Create new!
+              </Button>
+            </InputGroup>
+            </Row>
+            <Row>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Message</th>
+                  <th>Votes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allHellos ? allHellos.map((data, i) => (
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td>{data[0]}</td>
+                    <td>{data[1]}</td>
+                    <td><Button onClick={() => voteHello(i)}>Vote!</Button></td>
+                  </tr>
+                )) : <tr></tr>}
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
       </main>
 
       <footer>
